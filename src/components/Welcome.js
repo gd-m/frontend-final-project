a7dff03fae67325e5b6b64729a69b0e3b877711b
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-09-14 23:33:15 -0500

    Welcome for non-logged in user

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div>
+            <div>
+                <h1>
+                    Pretend This is some Catchy Line to make you Stay on this Page
+                </h1>
+                <p>To know This application please <a href="/login">Login</a> or <a href="/signup">Signup</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
