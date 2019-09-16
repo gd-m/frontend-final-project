330471216dd24d1d3d56a2c3627a38ad34f97578
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-09-16 12:54:21 -0500

    added link for individual card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const BookCard = (props) => {
     return (
+        <Link to={`/books/${props.books.id}`} className="book-links">
         <div className="book-card" key={props.books.id}>
            <h3 className="book-card-title"> {props.books.title}</h3>
            <p className="book-card-author">Author: {props.books.author}</p>
-        </div>
+        </div></Link>
     )
 } 
 
