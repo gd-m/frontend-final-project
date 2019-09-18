aeca673ad42a7f1ccccf427deb9ebb9529009b67
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-09-18 00:59:46 -0500

    error into selecting a book in array, it has been corrected. parsed id into int

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,19 +8,18 @@ class Book extends Component {
     }
    componentDidMount(){
        let id
-       id = this.props.match.params.book_id
+       id = parseInt(this.props.match.params.book_id)
        console.log(id)
        console.log(this.props.location)
        console.log(this.props.match)
        let books
         if (this.props.currentUser) {
-            books = this.props.currentUser.attributes.books.books[id-1]
+            books = this.props.currentUser.attributes.books.books.find(book => book.id === id)
+            console.log(typeof(id))
             this.setState({
                 book: books
             })
         }
-        console.log(books[id-1])
-        console.log(books[6])
 
 
    }
@@ -29,6 +28,7 @@ class Book extends Component {
         return ( 
             <div className="book-show-page">
                <div className="book-info">
+                   {console.log(this.state)}
                     <h1>{this.state.book.title}</h1>
                     <h4>By : {this.state.book.author}</h4>
                </div>
