9992584893267e9772ced4d528f8cf4f3a8cebda
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-09-17 23:04:21 -0500

    Book Show page working

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 
 class Book extends Component {
+    state = {
+        book: []
+    }
    componentDidMount(){
        let id
        id = this.props.match.params.book_id
        console.log(id)
        console.log(this.props.location)
        console.log(this.props.match)
+       let books
+        if (this.props.currentUser) {
+            books = this.props.currentUser.attributes.books.books[id-1]
+            this.setState({
+                book: books
+            })
+        }
+        console.log(books[id-1])
+        console.log(books[6])
+
 
    }
-    render() { 
+    render() {
+        
         return ( 
-            <div>
-                
+            <div className="book-show-page">
+               <div className="book-info">
+                    <h1>{this.state.book.title}</h1>
+                    <h4>By : {this.state.book.author}</h4>
+               </div>
             </div>
          );
     }
 }
+
+const mapStateToProps = (state) => {
+    return ({
+        currentUser: state.currentUser
+    })
+}
  
-export default Book;
+export default connect(mapStateToProps) (Book)
